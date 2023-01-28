d366f76877bd3e1b0f9ad4b43ba7d0f0d6ebb162
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-01-28 15:05:28 +0530

    Added required files

---
+++ learnreact/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+let name="Aayushi";
+function App() {
+  
+  return (
+    <>
+   
+   <nav>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Services</li>
+      <li>Blog</li>
+    </ul>
+   </nav>
+   <img src='https://images.unsplash.com/photo-1611262588024-d12430b98920?w=720&h=1280&fit=crop'></img>
+   <div className='container'>
+    <p>Hello {name}</p>
+    <p>lAukjfsjkfil kdgjkldsgk;p ek;orkjfmsd. kgfirscmg fdkgjl.bg;l;l</p>
+    
+   </div>
+    </>
+    
+  );
+}
+
+export default App;
