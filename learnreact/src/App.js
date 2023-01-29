b8f8a71b99c86c7c75af9f4fbe9df4d2126c4415
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-01-29 20:14:31 +0530

    body dark mode

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import TextForm from './Components/TextForm';
 import Navbar from './Components/Navbar';
 import About   from './Components/About';
+import { useState } from 'react';
 function App() {
-
+ const [mode, setMode]=  useState('light'); //wheather dark mode  enable or not
+ const toggleMode=()=>{
+  if(mode=== "light"){
+    setMode("dark")
+    document.body.style.backgroundColor="black"
+  }
+  else{
+    setMode("light")
+    document.body.style.backgroundColor="white"
+  }
+ }
   return (
     <>
-      <Navbar title="Title"  about=" About"/>
+      <Navbar title="Title" mode={mode} toggleMode={toggleMode} />
       <div className='container my-4'>
         
         <TextForm heading="Enter your text"/></div>
